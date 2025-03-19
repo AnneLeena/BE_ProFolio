0d9d1ab272824624be024dd496a5654edcd1ff1b
unknown <bhd683@myy.haaga-helia.fi>
2025-03-19 21:59:54 +0200

    Projekti luotu

---
+++ src/main/java/backend/profolio/web/MyController.java
@@ -0,0 +1,16 @@
+package backend.profolio.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/main")
+    @ResponseBody
+    public String returnMessage() {
+        return "Tästä se alkaa";
+    }
+
+}
